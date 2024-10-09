aec8c7cc6617695a85f40bde5d341ac839868f50
ZurrBotz <159263248+ZurrBotz@users.noreply.github.com>
2024-10-09 12:34:02 +0700

    Update PortfolioView.vue

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -11,7 +11,7 @@ export default {
           status: 'My personal website, I created this website to display my profile, skills and projects. As well as my place to try new technology.',
           tech: 'VueJS 3, Tailwind',
           github: 'https://github.com/ZurrBotz/Portofolio-v2',
-          demo: 'https:/'
+          demo: 'https://zuraportf.netlify.app'
         },
         {
           id: 2,
